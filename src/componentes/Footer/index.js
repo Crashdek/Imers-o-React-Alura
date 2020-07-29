48ffb139c49d7e3ab9629b180ce12217a999a939
Crashdek <erick.bragante@hotmail.com>
2020-07-29 01:47:55 -0300

    SPA implementado

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
+import './footer.css'
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://www.linkedin.com/in/erick-bragante/" target="_blank"  rel="noopener noreferrer">
+        <img className="Logo" src={Logo} alt="Logo ErickFlix" />
       </a>
       <p>
         Orgulhosamente criado durante a

0fc418b49deb0432ae9b662a38498f9c336dd0e8
Crashdek <66188942+Crashdek@users.noreply.github.com>
2020-08-02 02:35:49 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACK = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https:/erickflix.herokuapp.com/';
+  : 'https://erickflix.herokuapp.com';
 
 export default {
   URL_BACK,
